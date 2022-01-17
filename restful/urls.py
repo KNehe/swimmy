27392f1ce5b92015644d8a6d20b4042877b08a01
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-01-17 19:04:03 +0300

    jwt:user login and registration

---
+++ restful/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+from .views import RegisterAPIView, MyTokenObtainPairView
+
+urlpatterns = [
+    path('users/login/', MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/register/', RegisterAPIView.as_view(), name='register_user')
+]
--- swimmy/urls.py
+++ swimmy/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+base_url = 'api/v1/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(f'{base_url}', include('restful.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
