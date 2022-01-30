a7d72024e8d9176a35a9e7cdc44be33e2a0358f2
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-01-30 12:41:38 +0300

    setup CORS

--- swimmy/settings.py
+++ swimmy/settings.py
@@ -51,9 +51,11 @@ INSTALLED_APPS = [
     'customuser.apps.CustomuserConfig',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -201,3 +203,5 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+CORS_ALLOWED_ORIGINS = env('CORS_ALLOWED_ORIGINS').split(',')
